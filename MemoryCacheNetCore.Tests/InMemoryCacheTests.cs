d99b12d582ab54f4e5f304a60a8f6fa3fb6a0f1e
Aymen TROUDI <aymen.troudi@younited-credit.fr>
2019-04-27 17:08:58 +0200

    Add ReaderWriterLockSlim (to avoid disposed token when using cache)

--- MemoryCacheNetCore.Tests/InMemoryCacheTests.cs
+++ MemoryCacheNetCore.Tests/InMemoryCacheTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,25 +8,21 @@ namespace MemoryCacheNetCore.Tests
     [TestClass]
     public class InMemoryCacheTests
     {
-        private const int LessThanExpiration = 1000;
         private const int MoreThanExpiration = 6000;
+        private static readonly Random Random = new Random();
 
         [TestMethod]
         public async Task Get_Old_Value_When_Delay_Is_Not_Expired()
         {
-            var cache = new InMemoryCache($"{DateTime.UtcNow}");
+            var cache = new InMemoryCache();
 
             var xx = await cache.AddOrGetAsync("foo",
-                async () => await Task.FromResult(10))
-                .ConfigureAwait(false);
+                async () => await Task.FromResult(10));
 
             Assert.AreEqual(10, xx);
 
-            await Task.Delay(LessThanExpiration);
-
             var yy = await cache.AddOrGetAsync("foo",
-                async () => await Task.FromResult(20))
-                .ConfigureAwait(false);
+                async () => await Task.FromResult(20));
 
             Assert.AreEqual(10, yy);
         }
@@ -33,7 +30,7 @@ namespace MemoryCacheNetCore.Tests
         [TestMethod]
         public async Task Get_New_Value_When_Delay_Is_Expired()
         {
-            var cache = new InMemoryCache($"{DateTime.UtcNow}");
+            var cache = new InMemoryCache();
 
             var xx = await cache.AddOrGetAsync("foo",
                 async () => await Task.FromResult(10));
@@ -51,44 +48,77 @@ namespace MemoryCacheNetCore.Tests
         [TestMethod]
         public async Task Get_New_Value_When_Delay_Is_Not_Expired_And_Cache_Was_Flushed()
         {
-            var cache = new InMemoryCache($"{DateTime.UtcNow}");
+            var cache = new InMemoryCache();
 
             var xx = await cache.AddOrGetAsync("foo",
-                async () => await Task.FromResult(10))
-                .ConfigureAwait(false);
+                async () => await Task.FromResult(10));
 
             Assert.AreEqual(10, xx);
 
             cache.ClearCacheEntries();
 
             var yy = await cache.AddOrGetAsync("foo",
-                async () => await Task.FromResult(20))
-                .ConfigureAwait(false);
+                async () => await Task.FromResult(20));
 
             Assert.AreEqual(20, yy);
         }
 
         [TestMethod]
-        public async Task Should_Not_Throw_Exception_When_Get_Or_Add_Or_Flush_Cache()
+        public async Task Should_Not_Throw_Exception_When_Cache_Is_Flushed_While_Is_Used_V1()
         {
-            var cache = new InMemoryCache($"{DateTime.UtcNow}");
-
-            var task1 = cache.AddOrGetAsync("toto",
-                async () => await Task.FromResult(10));
-
-            var task2 = cache.AddOrGetAsync("toto",
-                async () => await Task.FromResult(20));
-
-            var task3 = Task.Run(() => cache.ClearCacheEntries());
-
             try
             {
-                await Task.WhenAll(task1, task2, task3).ConfigureAwait(false);
+                await Task.WhenAll(RandomCacheTasks());
             }
             catch (Exception ex)
             {
                 Assert.Fail(ex.Message);
             }
+        }
+
+        [TestMethod]
+        public void Should_Not_Throw_Exception_When_Cache_Is_Flushed_While_Is_Used_V2()
+        {
+            var cache = new InMemoryCache();
+
+            Parallel.For(1, 100, async index =>
+            {
+                await Task.Delay(Random.Next(100));
+
+                var key = Guid.NewGuid().ToString();
+
+                var xx = await cache.AddOrGetAsync(key,
+                    async () => await Task.FromResult(10));
+
+                Assert.AreEqual(10, xx);
+
+                cache.ClearCacheEntries();
+
+                var yy = await cache.AddOrGetAsync(key,
+                    async () => await Task.FromResult(20));
+
+                Assert.AreEqual(20, yy);
+            });
+        }
+
+        private static Task[] RandomCacheTasks()
+        {
+            var cache = new InMemoryCache();
+
+            var tasks1 = Enumerable.Range(1, 20)
+                .Select(x => cache.AddOrGetAsync($"foo-{x}",
+                    async () => await Task.FromResult(x)));
+
+            var tasks2 = Enumerable.Range(1, 20)
+                .Select(x => cache.AddOrGetAsync($"bar-{x}",
+                    async () => await Task.FromResult(x)));
+
+            var tasks3 = Enumerable.Range(1, 10)
+                .Select(x => Task.Run(() => cache.ClearCacheEntries()));
+
+            var array = tasks1.Union(tasks2).Union(tasks3);
+
+            return array.OrderBy(x => Random.Next()).ToArray();
         }
     }
 }
--- MemoryCacheNetCore/InMemoryCache.cs
+++ MemoryCacheNetCore/InMemoryCache.cs
@@ -8,54 +8,70 @@ namespace MemoryCacheNetCore
 {
     public sealed class InMemoryCache : IDisposable
     {
-        private readonly string _name;
         private readonly MemoryCache _cache;
         private const int CacheSecondsDuration = 5;
+        private static readonly ReaderWriterLockSlim Locker = new ReaderWriterLockSlim();
         private static CancellationTokenSource _flushCacheToken = new CancellationTokenSource();
 
-        public InMemoryCache(string name)
+        public InMemoryCache()
         {
-            _name = name;
             _cache = new MemoryCache(new MemoryCacheOptions());
         }
 
-        public async Task<T> AddOrGetAsync<T>(string key, Func<Task<T>> func)
+        public Task<T> AddOrGetAsync<T>(string key, Func<Task<T>> func)
         {
-            var newValue = new Lazy<Task<T>>(func);
             var expiration = DateTimeOffset.UtcNow.AddSeconds(CacheSecondsDuration);
-            var oldValue = _cache.GetOrCreate(key, entry =>
-            {
-                entry.AbsoluteExpiration = expiration;
-                entry.AddExpirationToken(new CancellationChangeToken(_flushCacheToken.Token));
-                entry.RegisterPostEvictionCallback(EvictionCallback);
-                return newValue;
-            });
+            return AddOrGetAsync(key, func, expiration);
+        }
+
+        public async Task<T> AddOrGetAsync<T>(string key, Func<Task<T>> func, DateTimeOffset expiration)
+        {
+            Locker.EnterReadLock();
+
             try
             {
-                return await (oldValue ?? newValue).Value;
+                var newValue = new Lazy<Task<T>>(func);
+                var oldValue = _cache.GetOrCreate(key, entry =>
+                {
+                    entry.AbsoluteExpiration = expiration;
+                    entry.AddExpirationToken(new CancellationChangeToken(_flushCacheToken.Token));
+                    entry.RegisterPostEvictionCallback(EvictionCallback);
+                    return newValue;
+                });
+                try
+                {
+                    return await (oldValue ?? newValue).Value;
+                }
+                catch
+                {
+                    _cache.Remove(key);
+                    throw;
+                }
             }
-            catch
+            finally
             {
-                _cache.Remove(key);
-                throw;
+                Locker.ExitReadLock();
             }
         }
 
         public void ClearCacheEntries()
         {
-            if (CanCancelToken(_flushCacheToken))
+            if (!CanCancelToken(_flushCacheToken)) return;
+
+            Locker.EnterWriteLock();
+
+            try
             {
                 _flushCacheToken.Cancel();
                 _flushCacheToken.Dispose();
                 _flushCacheToken = new CancellationTokenSource();
             }
+            finally
+            {
+                Locker.ExitWriteLock();
+            }
 
             Console.WriteLine("Flushing cache");
-        }
-
-        public override string ToString()
-        {
-            return _name;
         }
 
         public void Dispose()
